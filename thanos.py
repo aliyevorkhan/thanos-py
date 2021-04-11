e90510ad9b65caf1e680cde08db799b32051d609
Orkhan ALIYEV <338396@ogr.ktu.edu.tr>
2021-04-11 21:03:59 +0400

    Update thanos.py

--- thanos.py
+++ thanos.py
@@ -2,6 +2,8 @@ import sys
 from glob import glob
 import os
 import random
+import sounddevice as sd
+import numpy as np
 
 def snap(dir):
     files = glob(dir+'/*')
@@ -10,5 +12,13 @@ def snap(dir):
         if count%2==0:
             os.remove(file)
 
+def print_sound(indata, outdata, frames, time, status):
+    volume_norm = np.linalg.norm(indata)*10
+    if int(volume_norm)>200:
+        print ("You have been deleted half of the all files!")
+        snap(str(sys.argv[1]))
+
 if __name__=='__main__':
-    snap(str(sys.argv[1]))
+    
+    with sd.Stream(callback=print_sound):
+        sd.sleep(int(sys.argv[2]))
